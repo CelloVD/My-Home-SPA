d0c79305e0d005e6ec1ef5932501e0574b91a0e2
CelloVD <165874221+CelloVD@users.noreply.github.com>
2026-06-16 19:38:47 -0400

    Add files via upload
    Added Tilin

---
+++ MyHome_SPA/src/Personal/Cliente.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personal;
+
+/**
+ *
+ * @author Tito
+ */
+public class Cliente extends Personal {
+
+    private String correo;
+
+    public Cliente() {
+    }
+
+    public Cliente(String correo) {
+        this.correo = correo;
+    }
+
+    public Cliente(String correo, String rut, String nombre_Completo) {
+        super(rut, nombre_Completo);
+        this.correo = correo;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "correo=" + correo + '}';
+    }
+    
+    
+    
+    
+    
+}
